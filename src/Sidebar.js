5199e60a4312af463f004ac62c863a22a941776e
Olebogeng Sebogodi <sebogodio@icloud.com>
2023-12-18 17:01:12 +0200

    dashboard

---
+++ src/Sidebar.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import{BsCart3, BsFillArchiveFill, BsFillGearFill, BsFillGrid3X3GapFill, BsGrid1X2Fill, BsListCheck, BsMenuButtonWideFill, BsPeopleFill} from "react-icons/bs"
+
+
+function Sidebar({openSidebarToggle, OpenSidebar}) {
+  return (
+    <aside id="sidebar" className={openSidebarToggle ? "sb-responsive" : ""}>
+      <div className='sb-title'>
+        <div className='sb-brand'>
+          <BsCart3 className='icon_header'/> Shop
+        </div>
+        <span className='icon close-icon' onClick={OpenSidebar}>X</span>
+      </div>
+      <ul className='sb-list'>
+        <li className='sb-list-item'>
+          <a href=''>
+            <BsGrid1X2Fill className='icon'/> Dashboard
+          </a>
+        </li>
+        <li className='sb-list-item'>
+          <a href=''>
+            <BsFillArchiveFill className='icon'/> Products
+          </a>
+        </li>
+        <li className='sb-list-item'>
+          <a href=''>
+            <BsFillGrid3X3GapFill className='icon'/> Categories
+          </a>
+        </li>
+        <li className='sb-list-item'>
+          <a href=''>
+            <BsPeopleFill className='icon'/> Customers
+          </a>
+        </li>
+        <li className='sb-list-item'>
+          <a href=''>
+            <BsListCheck className='icon'/> Inventory
+          </a>
+        </li>
+        <li className='sb-list-item'>
+          <a href=''>
+            <BsMenuButtonWideFill className='icon'/> Reports
+          </a>
+        </li>
+        <li className='sb-list-item'>
+          <a href=''>
+            <BsFillGearFill className='icon'/> Setting
+          </a>
+        </li>
+      </ul>
+    </aside>
+  )
+}
+
+export default Sidebar
